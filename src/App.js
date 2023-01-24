15c20a914211174e9ba5da68e3dd5daa3dc59c5f
Goushith <goushithcmohan@gmail.com>
2023-01-24 14:15:57 +0530

    counterapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter,setCounter]=useState(0) //state - counter - To hold value
+
+function increment(){
+  setCounter(counter+1)
+}
+
+
+function decrement(){
+  if(counter!=0)
+  setCounter(counter-1)
+  
+}
+
+function reset(){
+  setCounter(0)
+}
+
   return (
     <div className="App">
+
+<nav class="navbar"  >
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1 text-dark">Counter Application</span>
+  </div>
+</nav>
+
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Your Turn 😉</h1>
+       <h1>{counter}</h1>
+      <div className='buttondiv'>
+       <button className='button-33'  onClick={increment}>Addition</button> &nbsp;
+       <button  className='button-34' onClick={decrement}>Subtract</button> &nbsp;
+       <button  className='button-35' onClick={reset}>Reset</button>
+       </div>
       </header>
+      
     </div>
   );
 }
